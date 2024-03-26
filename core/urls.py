950ff37c4eb11b5293d8d63126ab4d55e1a9a5f5
Regis Orate <ott.regisorate@gmail.com>
2024-03-26 07:29:46 +0000

    Change core urls.py for debug toolbar

--- core/urls.py
+++ core/urls.py
@@ -17,13 +17,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 
 """
-
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('__debug__/', include('debug_toolbar.urls')),
     # Local apps
     path('hello/', include('apps.hello.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [path('__debug__/', include('debug_toolbar.urls'))]
