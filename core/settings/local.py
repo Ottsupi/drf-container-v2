b3fe871f53b0b54647d26ba93f4c99ac8ba2c9d0
Regis Orate <ott.regisorate@gmail.com>
2024-03-26 10:52:57 +0000

    Change local to use env.db

--- core/settings/local.py
+++ core/settings/local.py
@@ -1,6 +1,10 @@
 import socket
 
+import environ
+
 from .base import *  # noqa: F403
+
+env = environ.Env()
 
 DEBUG = True
 
@@ -8,12 +12,14 @@ ALLOWED_HOSTS = [
     '0.0.0.0',  # noqa: S104
     'localhost',
     '127.0.0.1',
+    env('HOST_NAME'),
 ]
 
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost',
     'http://127.0.0.1',
+    env('HOST_NAME'),
 ]
 
 
@@ -33,16 +39,7 @@ INTERNAL_IPS = [ip[: ip.rfind('.')] + '.1' for ip in ips] + ['127.0.0.1', '10.0.
 
 
 # Database
-DATABASES = {
-    'default': {
-        'ENGINE': env('DATABASE_ENGINE'),
-        'NAME': env('DATABASE_NAME'),
-        'USER': env('DATABASE_USER'),
-        'PASSWORD': env('DATABASE_PASSWORD'),
-        'HOST': env('DATABASE_HOST'),
-        'PORT': env('DATABASE_PORT'),
-    }
-}
+DATABASES = {'default': env.db()}
 
 
 # Staticfiles
